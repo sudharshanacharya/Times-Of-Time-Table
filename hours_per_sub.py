1c7393a63c14e6e94a0964b8bab2b4af3f34835c
sudharshan <49453742+sudharshanacharya@users.noreply.github.com>
2021-01-06 00:13:56 +0530

    Add files via upload

---
+++ hours_per_sub.py
@@ -0,0 +1,58 @@
+import sqlite3
+
+conn = sqlite3.connect("database/TimesOfTimeTable.db")
+cur = conn.cursor()
+
+def count(sub_type, sem):
+    cur.execute("select count(sub_type) from subjects where sub_type = ? and sem = ?", (sub_type, sem,))
+    return cur.fetchall()
+
+
+class calculate_hours:
+    """total hours per subject"""
+
+    def __init__(self, sem):
+        self.sem = sem
+        self.theory = count("theory", sem)[0][0]
+        self.not_theory = count("not_theory", sem)[0][0]
+        self.labs = count("lab", sem)[0][0]
+        self.minor_subs = count("minor", sem)[0][0]
+        self.major_subs = count("major", sem)[0][0]
+        self.spl = count("spl", sem)[0][0]
+        self.total_hours_per_week = 39
+
+    def calculate(self):
+        total_subs = self.major_subs + self.not_theory + self.theory
+        print("total_subs", total_subs)
+        hours = (self.total_hours_per_week - (self.labs * 3 + self.minor_subs + self.spl * 2)) / total_subs
+        print("hours in general", hours)
+        per_sub = int(hours)
+        print("hours per sub", per_sub)
+        extra = (hours - per_sub) * total_subs
+        print("remaining extra hours",extra)
+        extra = round(extra)
+        print(extra)
+        per_major = per_sub
+        per_not_theory = per_sub
+        per_theory = per_sub
+        if extra and extra >= self.major_subs:
+            per_major = per_sub + 1
+            extra = extra - self.major_subs
+            print("extra", extra)
+        if extra and extra >= self.not_theory:
+            print("hello")
+            per_not_theory = per_sub + 1
+            extra = extra - self.not_theory
+        print(
+            per_major * self.major_subs + per_not_theory * self.not_theory + per_theory * self.theory + self.labs * 3 + self.minor_subs + self.spl * 2)
+        if (
+                per_major * self.major_subs + per_not_theory * self.not_theory + per_theory * self.theory + self.labs * 3 + self.minor_subs + self.spl * 2) <= 39:
+            return per_major, per_not_theory, per_theory
+
+
+cal = calculate_hours(5)
+
+m, nt, t = cal.calculate()
+
+print(m, nt, t, )
+conn.close()
---
+++ sub_input.py
@@ -0,0 +1,33 @@
+import sqlite3
+from lib.queries import queries
+
+
+class sub:
+    """take subject detail, Enters into subject database"""
+
+
+def sub_input(no_sub, drop_table = False, create_table = False):
+    conn = sqlite3.connect("database/TimesOfTimeTable.db")
+    cur = conn.cursor()
+    if drop_table:
+        queries.drop_table('subjects')
+        # cur.execute("drop table if EXISTS subjects ")
+        print("Table Dropped")
+    if create_table:
+        cur.execute(queries.create_table_subjects)
+
+    for i in range(no_sub):
+        print("subject (?)", i)
+        sub_name = input("Enter sub_nam: ")
+        sub_short_name = input("Enter sub_short_name: ")
+        sub_code = input("Enter sub_code: ")
+        sub_credit = input("Enter sub_credit: ")
+        sub_type = input("Enter sub_type: ")
+
+        cur.execute("""
+        insert into subjects (sub_short_name,sub_name, sub_code, sub_credit, sub_type)
+        values(?,?,?,?,?)
+        """, (sub_short_name, sub_name, sub_code, sub_credit, sub_type,))
+
+    conn.commit()
+    conn.close()
