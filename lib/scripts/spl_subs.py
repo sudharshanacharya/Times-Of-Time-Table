be920ffaaf4360a5025f50162e691823c6f5aabb
sudharshan <49453742+sudharshanacharya@users.noreply.github.com>
2021-01-17 08:09:24 +0530

    Add files via upload

---
+++ lib/scripts/spl_subs.py
@@ -0,0 +1,39 @@
+class spl:
+    """ """
+    def Spl(self, sub1, position):
+        highest = max(sem5.pos_a, sem5.pos_b, sem5.pos_c)
+        if highest in [0, 2, 4]:
+            for loop in (highest - sem5.pos_a):
+                if sem5.pos_a < highest:
+                    sub2 = get_one_sub(position)
+                    a.append(sub2)
+                    class_engaged[a_fac[sub2]] += 1
+                    a_hours[sub2] -= 1
+                    sem5.pos_a += 1
+
+                if sem5.pos_b < highest:
+                    sub2 = get_one_sub(position)
+                    b.append(sub2)
+                    class_engaged[b_fac[sub2]] += 1
+                    b_hours[sub2] -= 1
+                    sem5.pos_b += 1
+
+                if sem5.pos_c < highest:
+                    sub2 = get_one_sub(position)
+                    c.append(sub2)
+                    class_engaged[c_fac[sub2]] += 1
+                    c_hours[sub2] -= 1
+                    sem5.pos_c += 1
+
+            for i in range(2):
+                a.append(sub1)
+                b.append(sub1)
+                c.append(sub1)
+                class_engaged[b_fac[sub1]] += 1
+                sem5.pos_a += 1
+                sem5.pos_b += 1
+                sem5.pos_c += 1
+                a_hours[sub1] -= 1
+                b_hours[sub1] -= 1
+                c_hours[sub1] -= 1
+
---
+++ lib/scripts/variables.py
@@ -0,0 +1,29 @@
+class variables:
+    """ x_fac[sub] = sub faculty for class x """
+    a_fac = dict()
+    b_fac = dict()
+    c_fac = dict()
+
+    class_engaged = dict()
+
+    """ x_hours[sub] = remaining hours of sub """
+    a_hours = dict()
+    b_hours = dict()
+    c_hours = dict()
+    """ list of subjects """
+    subs = list()
+
+
+
+    """ list of lab engaged """
+    lab_not_engaged = list()
+    lab_not_engaged.append('lab1')
+    lab_not_engaged.append('lab2')
+    lab_not_engaged.append('lab3')
+
+    """ not usual class hours"""
+    if_lab = list()
+    if_lab.append('lab1')
+    if_lab.append('lab2')
+    if_lab.append('lab3')
+    if_lab.append('spl')
